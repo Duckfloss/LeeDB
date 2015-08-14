8d81fb99d6d51bc0b7d6a0df036a17d4e814d4fe
BenCJones <ben@bencjones.com>
2015-08-14 15:52:26 -0400

    Dunno. Who cares.

--- lib/test_data.rb
+++ lib/test_data.rb
@@ -3,8 +3,8 @@ $file = "data/UniteU_Departments_20150710132002.csv"
 $uid = { "id" => "1234" }
 $csv_table = "departments"
 $db_table = "categories"
-$this_map = build_map($csv_table)
-$this_schema = $db_schema[:"#{$db_table}"]
-$key = $this_schema[:KEY]
-$key = $key.split("-") if $key.match("-")
-$db = SQLite3::Database.new "#{$db_file}"
+#$this_map = build_map($csv_table)
+#$this_schema = $db_schema[:"#{$db_table}"]
+#$key = $this_schema[:KEY]
+#$key = $key.split("-") if $key.match("-")
+#$db = SQLite3::Database.new "#{$db_file}"
