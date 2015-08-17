6e244148eb70b10b1120c3353683836366e9e75b
BenCJones <ben@bencjones.com>
2015-08-17 16:27:04 -0400

    Adds Record module for mixins

--- lib/records.rb
+++ lib/records.rb
@@ -1,95 +1,87 @@
+# Shared record methods
+module Record
+  # Builds map
+  def Record.build_map(table)
+    mapfile = JSON.parse(File.read('lib/uniteu_leedb_map.json'), :symbolize_names=>true)
+    map = Hash.new
+  	mapfile[:"#{table}"][:fields].each { |h,k| map["#{h}"] = "#{k}" unless k==nil }
+  	return map
+  end
+end
 
 class Category
+  include Record
+
   # Constants
   KEY="id"
   UNITEUTABLE="departments"
 
   # Constructor
   def initialize
-    @map = build_map
+    @map = Record.build_map(UNITEUTABLE)
   end
-
-
 
   # Accessors
   def getMap
     @map
   end
 
-  def getDBTableName
-    @db_table_name
-  end
-
-  def getUniteUTableName
-    @uniteu_table_name
-  end
-
-  def getDBColumnNames
-    @db_column_names
-  end
-
-  def getUniteUColumnNames
-    @uniteu_column_names
-  end
-
-  # Methods
-  # Builds map
-  def build_map
-    mapfile = JSON.parse(File.read('lib/uniteu_leedb_map.json'), :symbolize_names=>true)
-    map = Hash.new
-  	mapfile[:"#{UNITEUTABLE}"][:fields].each { |h,k| map["#{h}"] = "#{k}" unless k==nil }
-  	return map
-  end
-
-  # Puts a row of UniteU data into a hash with database columns as keys
-  def getUUData(row)
-    data = {
-      "id" => row["dept_id"],
-	    "name" => row["dept_name"],
-	    "parent" => row["parent_id"],
-	    "description" => row["dept_description"],
-	    "img" => row["dept_image_1_file"],
-	    "rpro_id" => row["rpro_dept_id"]
-    }
-  end
-
-  # Puts a row of database data into a hash with UniteU columns as keys
-  def getDBData(row)
-    data = {
-      "dept_id" => row["id"],
-	    "dept_name" => row["name"],
-	    "parent_id" => row["parent"],
-	    "dept_description" => row["description"],
-	    "dept_image_1_file" => row["img"],
-	    "rpro_dept_id" => row["rpro_id"]
-    }
-  end
-
 end
 
 class Product
+  include Record
+
+  # Constants
+  KEY="id"
+  UNITEUTABLE="departments"
+
   # Constructor
   def initialize()
+    @map = Record.build_map(UNITEUTABLE)
+  end
 
+  # Accessors
+  def getMap
+    @map
   end
 
 
 end
 
 class Shopper
+  include Record
+
+  # Constants
+  KEY="id"
+  UNITEUTABLE="departments"
+
   # Constructor
   def initialize()
-
+    @map = Record.build_map(UNITEUTABLE)
   end
 
+  # Accessors
+  def getMap
+    @map
+  end
 
 end
 
 class Order
+  include Record
+
+  # Constants
+  KEY="id"
+  UNITEUTABLE="departments"
+
   # Constructor
   def initialize()
-
+    @map = Record.build_map(UNITEUTABLE)
   end
 
+  # Accessors
+  def getMap
+    @map
+  end
 
 end
