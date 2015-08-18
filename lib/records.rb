25300a460b972a67973079df7d1a2afbfd7023a0
BenCJones <ben@bencjones.com>
2015-08-18 11:30:32 -0400

    Adds Category class

--- lib/records.rb
+++ lib/records.rb
@@ -1,12 +1,28 @@
 # Shared record methods
 module Record
+
+  # Load db schema
+  DB_SCHEMA = JSON.parse(File.read('lib/leedb.json'), :symbolize_names=>true)
+  # Load db map
+  MAP = JSON.parse(File.read('lib/uniteu_leedb_map.json'), :symbolize_names=>true)
+
   # Builds map
   def Record.build_map(table)
-    mapfile = JSON.parse(File.read('lib/uniteu_leedb_map.json'), :symbolize_names=>true)
-    map = Hash.new
-  	mapfile[:"#{table}"][:fields].each { |h,k| map["#{h}"] = "#{k}" unless k==nil }
-  	return map
+    this_map = Hash.new
+  	MAP[:"#{table}"][:fields].each { |k,v| this_map["#{k}"] = "#{v}" unless v==nil }
+  	return this_map
   end
+
+  # Builds object attributes
+  def Record.build_attributes(table,data,map)
+    attributes = Hash.new
+    DB_SCHEMA[:"#{table}"][:FIELDS].each_key do |k|
+      this_map = map.invert["#{k}"]
+      attributes[:"#{k}"] = data[:"#{this_map}"]
+    end
+    return attributes
+  end
+
 end
 
 class Category
@@ -14,16 +30,20 @@ class Category
 
   # Constants
   KEY="id"
-  UNITEUTABLE="departments"
 
   # Constructor
-  def initialize
-    @map = Record.build_map(UNITEUTABLE)
+  def initialize(data)
+    @map = Record.build_map("departments")
+    @attributes = Record.build_attributes("categories",data,@map)
   end
 
   # Accessors
   def getMap
     @map
+  end
+
+  def getAttributes
+    @attributes
   end
 
 end
