2e0bf3da33bd3d50e009c34681530cc530d8f92c
BenCJones <ben@bencjones.com>
2015-08-24 15:55:46 -0400

    Updates data mapping for google shopping

--- lib/google_shopping.rb
+++ lib/google_shopping.rb
@@ -3,7 +3,7 @@ class GoogleShopping
 FILE = "LeesAdventureSportsFeed.txt"
 
 SCHEMA = JSON.parse(File.read('lib/schemas/google_shopping_schema.json'), :symbolize_names=>true)
-MAP = JSON.parse(File.read('lib/schemas/google_shopping_map.json'), :symbolize_names=>true)
+MAP = JSON.parse(File.read('lib/maps/google_shopping_map.json'), :symbolize_names=>true)
 
 def initialize(data)
 
