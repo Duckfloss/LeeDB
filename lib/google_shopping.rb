048683afe31e5b3705e101c71465929fda9e0765
BenCJones <ben@bencjones.com>
2015-08-24 15:40:50 -0400

    Adds Google_Shopping class

---
+++ lib/google_shopping.rb
@@ -0,0 +1,34 @@
+class GoogleShopping
+
+FILE = "LeesAdventureSportsFeed.txt"
+
+SCHEMA = JSON.parse(File.read('lib/schemas/google_shopping_schema.json'), :symbolize_names=>true)
+MAP = JSON.parse(File.read('lib/schemas/google_shopping_map.json'), :symbolize_names=>true)
+
+def initialize(data)
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
