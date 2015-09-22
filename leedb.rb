6694b4d52c13f6a469922771d10b8adeb5c44a89
BenCJones <ben@bencjones.com>
2015-09-22 16:59:18 -0400

    Now with more presets

--- leedb.rb
+++ leedb.rb
@@ -19,3 +19,13 @@ require 'test_data'
 # Load presets
 $db = "lee.db"
 $log = "logs/"+DateTime.now.strftime('%Y%m%d%H%M%S')+".txt"
+
+# Database field options
+$db_type = [ 'TEXT','REAL','INTEGER' ]
+$db_format = [ 'alpha16','char3','price','bool','url','int4','date','time','phone','email' ]
+$db_rule = [ 'KEY','NOTNULL','UNIQUE' ]
+
+# Load db schema
+$db_schema = JSON.parse(File.read('./lib/schemas/leedb.json'), :symbolize_names=>true)
+# Load db map
+$map = JSON.parse(File.read('./lib/maps/uniteu.json'), :symbolize_names=>true)
