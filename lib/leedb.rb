66f2044230509df7a7101c7bf433459844926af0
BenCJones <ben@bencjones.com>
2015-10-27 13:17:13 -0400

    Moves presets off repo

--- lib/leedb.rb
+++ lib/leedb.rb
@@ -13,17 +13,3 @@ require 'csv2db'
 require 'records'
 require 'rpro'
 require 'db'
-
-# Load presets
-$db = "../lee.db"
-$log = "../logs/"+DateTime.now.strftime('%Y%m%d%H%M%S')+".txt"
-
-# Database field options
-$db_type = [ 'TEXT','REAL','INTEGER' ]
-$db_format = [ 'alpha16','char3','price','bool','url','int4','date','time','phone','email' ]
-$db_rule = [ 'KEY','NOTNULL','UNIQUE' ]
-
-# Load db schema
-$db_schema = JSON.parse(File.read('./leedb/schemas/leedb.json'), :symbolize_names=>true)
-# Load db map
-$map = JSON.parse(File.read('./leedb/maps/uniteu.json'), :symbolize_names=>true)
