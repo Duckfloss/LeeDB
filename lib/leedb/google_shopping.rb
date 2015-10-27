781e8287702e4fc673a084c92151ea6773100530
BenCJones <ben@bencjones.com>
2015-10-27 13:30:22 -0400

    Updates more file links

--- lib/leedb/google_shopping.rb
+++ lib/leedb/google_shopping.rb
@@ -2,8 +2,8 @@ class GoogleShopping
 
 FILE = "LeesAdventureSportsFeed.txt"
 
-SCHEMA = JSON.parse(File.read('lib/schemas/google_shopping_schema.json'), :symbolize_names=>true)
-MAP = JSON.parse(File.read('lib/maps/google_shopping_map.json'), :symbolize_names=>true)
+SCHEMA = JSON.parse(File.read('leedb/schemas/google_shopping_schema.json'), :symbolize_names=>true)
+MAP = JSON.parse(File.read('leedb/maps/google_shopping_map.json'), :symbolize_names=>true)
 
 def initialize(data)
 
