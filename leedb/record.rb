95e376b9efa154890cf7a9eecaa36b3d0f72c992
BenCJones <ben@bencjones.com>
2015-12-18 11:55:51 -0500

    Modify inspect method

--- leedb/record.rb
+++ leedb/record.rb
@@ -23,9 +23,7 @@ class Record
 
   # Creates a record object
   def create_record(type)
-
     details = {}
-
     if TABLES.find_index(type).nil? # check if type is valid
       raise ArgumentError.new("Must indicate valid Record type")
     else
@@ -41,127 +39,19 @@ class Record
         end
       end
     end
-
     return details
-
   end
-
 
 
   def inspect
     output = ""
     if @type.length > 0
-      output << "@type = #{@type}\n"
-      output << "@details = #{@details}\n"
+      output << "<@type = #{@type} - "
+      output << "@details = #{@details.to_s.slice(0,60)}...>\n"
     else
-      output << "blank record\n"
+      output << "<blank record>\n"
     end
     return puts output
   end
 
 end
-
-
-
-=begin
-  class Category
-
-    # Constants
-    KEY="id"
-    TABLE="categories"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("departments")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = { "#{KEY}" => @attributes[:"#{KEY}"] }
-    end
-  end
-
-  class ProductGroup
-
-    # Constants
-    KEY="pf_id"
-    TABLE="product_groups"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("products")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = { "#{KEY}" => @attributes[:"#{KEY}"] }
-      @cat = [ @attributes.delete(:dept_id), @attributes[:pf_id] ]
-    end
-  end
-
-  class ProductItem
-
-    # Constants
-    KEY="sku"
-    TABLE="product_items"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("variants")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = { "#{KEY}" => @attributes[:"#{KEY}"] }
-    end
-  end
-
-  class Customer
-
-    # Constants
-    KEY="id"
-    TABLE="customers"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("shoppers")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = { "#{KEY}" => @attributes[:"#{KEY}"] }
-    end
-  end
-
-  class Order
-
-    # Constants
-    KEY="id"
-    TABLE="orders"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("salesorders")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = { "#{KEY}" => @attributes[:"#{KEY}"] }
-    end
-  end
-
-  class OrderItem
-
-    # Constants
-    KEY=["id","sku"]
-    TABLE = "order_items"
-
-    attr_reader :map, :attributes, :uid
-
-    # Constructor
-    def initialize(data)
-      @map = Record.build_map("salesorderssubitems")
-      @attributes = Record.build_attributes(TABLE,data,@map)
-      @uid = Hash.new
-      KEY.each do |i|
-        @uid["#{i}"] = @attributes[:"#{i}"]
-      end
-    end
-  end
-end
-=end
